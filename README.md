23297ca09e9bcd9e7f9f50bb72d70d967c7426f8
Ryan Chimienti <ryanchimienti@gmail.com>
2023-10-16 15:05:54 -0500

    Working end-to-end

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # genesys-voicebot-example
-A minimal proof-of-concept voicebot built with Deepgram's Genesys AudioHook integration
+A minimal proof-of-concept voicebot built with Deepgram's Genesys AudioHook integration.
+
+## How to run
+- Install the dependencies into a virtual environment:
+    ```shell
+    python3 -m venv venv
+    source ./venv/bin/activate
+    pip install -r requirements.txt
+    ```
+- Run the Flask server:
+    ```shell
+    flask run --with-threads
+    ```
+- Run an ngrok tunnel to `http://localhost:5000`, where the Flask server is listening
+- Install Deepgram's AudioHook integration into your Genesys org following [this guide](https://developers.deepgram.com/docs/genesys-with-deepgram)
+    - Recommended settings (swapping in your ngrok URL):
+        ```json
+        {
+            "endpointing": 10,
+            "callback": "https://YOUR-URL-HERE.ngrok-free.app/process-dg-callback/conversation/{conversation-id}",
+            "model": "nova",
+            "punctuate": true
+        }
+        ```
+- Import `get_main_intent_action.json` as a Web Services Data Action in your Genesys org
+    - Swap your ngrok URL into the **Request URL Template**
+    - Make sure the Web Services Data Action is installed and activated as an Integration under **Admin > Integrations**
+- Import `architect_flow.yaml` as an Inbound Call Flow in your Genesys Architect view
+- Associate the Architect Flow with a phone number under **Admin > Telephony > DID Numbers**
+- Call into the phone number and try out the flow!
+
+## Caveats
+This is a minimal proof of concept to demonstrate that a low-latency voicebot can be achieved with Deepgram's AudioHook integration. It has serious flaws that must be corrected before productionizing. In particular:
+
+- It assumes the user's reply is the first `speech_final` result. In reality, the reply may be spread over multiple results, or the reply may finish before a `speech_final` is received at all. A production bot should follow Deepgram's [best practices for utterance Segmentation](https://developers.deepgram.com/docs/understand-endpointing-interim-results#best-practices-for-utterance-segmentation).
+- Its threading logic should be improved to handle load.
--- app.py
+++ app.py
@@ -1,5 +1,20 @@
+import time
 from flask import Flask, request, make_response, jsonify
+import pylru
 import re
+import threading
+
+RECENT_TRANSCRIPTS_CACHE_SIZE = 1000
+
+# When listening for speech from the user, we ignore transcripts older than this
+TRANSCRIPT_EXPIRY_TIME_SEC = 1.0
+
+# LRU cache mapping from conversation id to a tuple of:
+#   1. Most recent transcript for that conversation ID
+#   2. Time that we received that transcript (in seconds from the epoch)
+recent_transcripts = pylru.lrucache(RECENT_TRANSCRIPTS_CACHE_SIZE)
+
+recent_transcripts_lock = threading.Lock()
 
 app = Flask(__name__)
 
@@ -32,13 +47,81 @@ def listen_main_intent():
         print("ERROR: conversation_id query parameter was not UUID")
         return "Conversation_id query parameter should be a UUID", 400
 
+    request_recv_time = time.time()
+    with recent_transcripts_lock:
+        if conversation_id in recent_transcripts:
+            _, transcript_recv_time = recent_transcripts[conversation_id]
+            if request_recv_time - transcript_recv_time > TRANSCRIPT_EXPIRY_TIME_SEC:
+                del recent_transcripts[conversation_id]
+
+    transcript = None
+    for _ in range(200):
+        with recent_transcripts_lock:
+            if conversation_id in recent_transcripts:
+                transcript, _ = recent_transcripts[conversation_id]
+
+                print(f"Found transcript: {transcript}")
+
+                del recent_transcripts[conversation_id]
+                break
+
+        # Wait 1/20 of a second between each of 200 checks, for a total waiting time of 10 seconds
+        time.sleep(0.05)
     
-    # TODO read utterance from Redis for this conversation id, and determine intent from utterance
+    if not transcript:
+        intent = "timeout"
+    elif "ride" in transcript or "book" in transcript:
+        intent = "book_ride"
+    elif "order" in transcript or "food" in transcript:
+        intent = "order_food"
+    else:
+        intent = "unknown"
 
     return make_response(
         jsonify(
             {
-                "intent": "book_ride"
+                "intent": intent
             }
         )
     )
+
+@app.post("/process-dg-callback/conversation/<conversation_id>")
+def process_dg_callback(conversation_id):   
+    req_body = request.get_json(force=True)
+
+    # print(f"Received DG callback: {req_body}")
+
+    uuid_regex = re.compile(
+        "^[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}$"
+    )
+    if not uuid_regex.match(conversation_id):
+        print("ERROR: conversation id received from Deepgram callback was not a UUID")
+        return "Conversation id was not a UUID", 400
+
+    if not "type" in req_body or not req_body["type"] == "Results":
+        # Ignore this callback because it doesn't contain transcripts
+        return "", 200
+    
+    if req_body["channel_index"][0] != 0:
+        # Ignore this callback because it's audio the caller hears, and we are only interested in
+        # audio the caller speaks
+        return "", 200
+    
+    if not req_body["speech_final"]:
+        # Ignore this callback because the caller hasn't finished speaking
+        return "", 200
+    
+    transcript = req_body["channel"]["alternatives"][0]["transcript"]
+
+    if not transcript:
+        # Ignore this transcript because it's empty
+        return "", 200
+
+    print(f"Received callback with a valid transcript: {transcript}")
+
+    transcript_recv_time = time.time()
+
+    with recent_transcripts_lock:
+        recent_transcripts[conversation_id] = transcript, transcript_recv_time
+
+    return "", 200
